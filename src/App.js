164b74d11a4de53008ed49d48cf4ae2328b584a3
nr <nicholas.raethke@gmail.com>
2018-10-23 23:11:51 -0700

    add mission statement

--- src/App.js
+++ src/App.js
@@ -61,6 +61,13 @@ class App extends Component {
           <h1 className="App-title">Little Block Book</h1>
           <image src={book} alt="Little Block Book Logo"></image>
         </header>
+        <h2 className='Welcome'>
+          Little Block Book is a decentralized dating app built in the BlockStack ecosystem designed bring authenticity back to the dating world.
+        </h2>
+        <p>
+          Little Block Book aims to solve the myriad of problems with current dating applications and sites. Little Block Book gives users ownership of their data and will never sell it. Little Block Book uses Blockstack's rigorous social identification system to make it harder bots, catfish, and stalkers to proliferate in the community. The Blockstack social level determines whether a user can participate in Little Block Book and each Blockstack account can only have one Little Block Book Profile. Users have a right to an alias within Little Block Book, but the Admins will be able to connect it back to a Blockstack profile if necessary.
+        </p>
+
         <p style={{display: this.state.isSignedIn ? 'none' : 'block' }}>
           <button onClick={this.handleSignIn}>
             Sign-in with Blockstack
